be9291ac750321e285d78dbf428a56ba78445449
terngkub <terngnattapol@gmail.com>
2025-06-30 17:27:41 +0700

    Migrate API Gateway to terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -229,3 +229,122 @@ resource "aws_lambda_function" "visitor_counter" {
   runtime       = local.lambda_runtime
   handler       = local.lambda_handler
 }
+
+# API Gateway
+
+locals {
+  api_path         = "increase-visitor-counter"
+  api_allow_origin = "https://resume.nattapol.com"
+}
+
+resource "aws_api_gateway_rest_api" "visitor_counter" {
+  name = "crc-visitor-counter"
+
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+}
+
+resource "aws_api_gateway_resource" "visitor_counter" {
+  rest_api_id = aws_api_gateway_rest_api.visitor_counter.id
+  parent_id   = aws_api_gateway_rest_api.visitor_counter.root_resource_id
+  path_part   = local.api_path
+}
+
+moved {
+  from = aws_api_gateway_method.visitor_counter
+  to   = aws_api_gateway_method.visitor_counter_post
+}
+
+moved {
+  from = aws_api_gateway_integration.visitor_counter
+  to   = aws_api_gateway_integration.visitor_counter_post
+}
+
+resource "aws_api_gateway_method" "visitor_counter_post" {
+  rest_api_id   = aws_api_gateway_rest_api.visitor_counter.id
+  resource_id   = aws_api_gateway_resource.visitor_counter.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "visitor_counter_post" {
+  rest_api_id             = aws_api_gateway_rest_api.visitor_counter.id
+  resource_id             = aws_api_gateway_resource.visitor_counter.id
+  http_method             = aws_api_gateway_method.visitor_counter_post.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.visitor_counter.invoke_arn
+}
+
+# Allow API Gateway to invoke the Lambda function
+resource "aws_lambda_permission" "visitor_count_post" {
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.visitor_counter.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.visitor_counter.execution_arn}/*"
+}
+
+## CORS
+
+resource "aws_api_gateway_method" "visitor_counter_options" {
+  rest_api_id   = aws_api_gateway_rest_api.visitor_counter.id
+  resource_id   = aws_api_gateway_resource.visitor_counter.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "visitor_counter_options" {
+  rest_api_id = aws_api_gateway_rest_api.visitor_counter.id
+  resource_id = aws_api_gateway_resource.visitor_counter.id
+  http_method = aws_api_gateway_method.visitor_counter_options.http_method
+  type        = "MOCK"
+}
+
+resource "aws_api_gateway_integration_response" "visitor_counter_options" {
+  depends_on = [
+    aws_api_gateway_integration.visitor_counter_options,
+    aws_api_gateway_method_response.visitor_counter_options
+  ]
+
+  rest_api_id = aws_api_gateway_rest_api.visitor_counter.id
+  resource_id = aws_api_gateway_resource.visitor_counter.id
+  http_method = aws_api_gateway_method.visitor_counter_options.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
+    "method.response.header.Access-Control-Allow-Methods" = "'OPTIONS,POST'"
+    "method.response.header.Access-Control-Allow-Origin"  = "'https://resume.nattapol.com'"
+  }
+}
+
+resource "aws_api_gateway_method_response" "visitor_counter_options" {
+  rest_api_id = aws_api_gateway_rest_api.visitor_counter.id
+  resource_id = aws_api_gateway_resource.visitor_counter.id
+  http_method = aws_api_gateway_method.visitor_counter_options.http_method
+  status_code = "200"
+
+  # TODO why empty?
+  response_models = {
+    "application/json" = "Empty"
+  }
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+}
+
+## Deployment
+
+resource "aws_api_gateway_deployment" "visitor_counter" {
+  rest_api_id = aws_api_gateway_rest_api.visitor_counter.id
+}
+
+resource "aws_api_gateway_stage" "visitor_counter_prod" {
+  rest_api_id   = aws_api_gateway_rest_api.visitor_counter.id
+  deployment_id = aws_api_gateway_deployment.visitor_counter.id
+  stage_name    = "prod"
+}
