e4c324cf2cd6009c3f4ad60dd3bd6c7c114ae0c1
terngkub <terngnattapol@gmail.com>
2025-07-14 16:02:05 +0700

    Add API Gateway to CloudFront

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,6 +25,14 @@ data "aws_cloudfront_cache_policy" "caching_optimized" {
   name = "Managed-CachingOptimized"
 }
 
+data "aws_cloudfront_cache_policy" "caching_disabled" {
+  name = "Managed-CachingDisabled"
+}
+
+locals {
+  api_gateway_origin_id = replace(aws_api_gateway_stage.visitor_counter_prod.invoke_url, "/^https?://([^/]*).*/", "$1")
+}
+
 data "aws_acm_certificate" "resume_website" {
   provider = aws.us-east-1
   domain   = var.root_domain_name
@@ -38,17 +46,40 @@ resource "aws_cloudfront_origin_access_control" "resume_website" {
 }
 
 resource "aws_cloudfront_distribution" "resume_website" {
-  origin {
-    domain_name              = aws_s3_bucket.resume_website.bucket_regional_domain_name
-    origin_access_control_id = aws_cloudfront_origin_access_control.resume_website.id
-    origin_id                = var.full_domain_name
-  }
-
   enabled             = true
   is_ipv6_enabled     = true
   http_version        = "http2and3"
   default_root_object = var.website_index_file
   aliases             = [var.full_domain_name]
+
+  viewer_certificate {
+    cloudfront_default_certificate = false
+    acm_certificate_arn            = data.aws_acm_certificate.resume_website.arn
+    minimum_protocol_version       = "TLSv1.2_2021"
+    ssl_support_method             = "sni-only"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+      locations        = []
+    }
+  }
+
+  tags = {
+    "Name" = var.full_domain_name
+  }
+  tags_all = {
+    "Name" = var.full_domain_name
+  }
+
+  # S3
+
+  origin {
+    domain_name              = aws_s3_bucket.resume_website.bucket_regional_domain_name
+    origin_access_control_id = aws_cloudfront_origin_access_control.resume_website.id
+    origin_id                = var.full_domain_name
+  }
 
   default_cache_behavior {
     allowed_methods        = ["GET", "HEAD"]
@@ -59,25 +90,28 @@ resource "aws_cloudfront_distribution" "resume_website" {
     compress               = true
   }
 
-  restrictions {
-    geo_restriction {
-      restriction_type = "none"
-      locations        = []
+  # API Gateway
+
+  origin {
+    domain_name = local.api_gateway_origin_id
+    origin_id = local.api_gateway_origin_id
+    
+    custom_origin_config {
+      http_port = 80
+      https_port = 443
+      origin_protocol_policy = "https-only"
+      origin_ssl_protocols = ["TLSv1.2"]
     }
   }
 
-  viewer_certificate {
-    cloudfront_default_certificate = false
-    acm_certificate_arn            = data.aws_acm_certificate.resume_website.arn
-    minimum_protocol_version       = "TLSv1.2_2021"
-    ssl_support_method             = "sni-only"
-  }
-
-  tags = {
-    "Name" = var.full_domain_name
-  }
-  tags_all = {
-    "Name" = var.full_domain_name
+  ordered_cache_behavior {
+    path_pattern = "/${aws_api_gateway_stage.visitor_counter_prod.stage_name}/*"
+    target_origin_id = local.api_gateway_origin_id
+    compress = true
+    viewer_protocol_policy = "redirect-to-https"
+    allowed_methods = ["GET", "HEAD", "OPTIONS", "PUT", "POST", "PATCH", "DELETE"]
+    cache_policy_id = data.aws_cloudfront_cache_policy.caching_disabled.id
+    cached_methods = ["GET", "HEAD"]
   }
 }
 
@@ -300,7 +334,6 @@ resource "aws_api_gateway_method_response" "visitor_counter_options" {
   http_method = aws_api_gateway_method.visitor_counter_options.http_method
   status_code = "200"
 
-  # TODO why empty?
   response_models = {
     "application/json" = "Empty"
   }
