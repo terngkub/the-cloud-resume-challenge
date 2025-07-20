dc84a5f443c1ae7cb56c0315dc291c7440973edc
terngkub <terngnattapol@gmail.com>
2025-07-21 00:40:22 +0700

    Remove unused providers from prod

--- terraform/environments/prod/providers.tf
+++ terraform/environments/prod/providers.tf
@@ -14,13 +14,3 @@ terraform {
 
   required_version = ">= 1.2.0"
 }
-
-provider "aws" {
-  region = "ap-southeast-7"
-}
-
-# Imported certificate has to be in us-east-1 for CloudFront
-provider "aws" {
-  region = "us-east-1"
-  alias  = "us-east-1"
-}
